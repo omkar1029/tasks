2818a4d3ef8403cb98092cd6574f303f2eb6ec0a
MSI\osp29 <omkar.poshatwar@byjus.com>
2021-08-19 13:35:24 +0530

    added task 4

---
+++ assets/task4/SliderSelection.ts
@@ -0,0 +1,31 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class SliderSelection extends cc.Component {
+
+    @property(cc.Slider)
+    slider: cc.Slider = null;
+
+    @property(cc.Node)
+    sliderHandle: cc.Node = null;
+
+    @property(cc.Node)
+    toggles: cc.Node[] = [];
+
+    noOfToggles:number = 4;
+
+    onLoad(){
+        this.sliderHandle.on(cc.Node.EventType.TOUCH_MOVE, function (event) {
+            console.log("moving");
+            this.showClosestToggle();
+        }, this)
+    }
+
+    showClosestToggle(){
+        for (let i = 0; i < this.toggles.length; i++) {
+            if(this.slider.progress < (i+1)/this.noOfToggles && this.slider.progress > (i/this.noOfToggles)){
+                this.toggles[i].getComponent(cc.Toggle).isChecked = true;
+            }
+        }
+    }
+}
