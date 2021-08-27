36e8bb6b52a194f0676642bfb11e66b26bc8ab8f
Omkar Poshatwar <omkarposhatwar12345@gmail.com>
2021-08-27 18:48:35 +0530

    added method to hit the ball thrown from player level to ground level target.

--- assets/task6/Target.ts
+++ assets/task6/Target.ts
@@ -31,6 +31,9 @@ export default class Target extends cc.Component {
     @property(cc.AnimationClip)
     throwSequence: cc.AnimationClip = null;
 
+    @property
+    groundY: number = -162;
+
     onLoad() {
         cc.director.getPhysicsManager().enabled = true;
 
@@ -42,11 +45,13 @@ export default class Target extends cc.Component {
             let loc: cc.Vec2 = event.getLocation();
             let loc2: cc.Vec2 = this.canvas.convertToNodeSpaceAR(loc);
 
-            this.mark.setPosition(loc2.x, this.shootPoint.y);
+            //this.mark.setPosition(loc2.x, this.shootPoint.y);//for calculateVelocityPlayerLevelTarget
+            this.mark.setPosition(loc2.x, this.groundY);//for calculateVelocityGroundLevelTarget
             this.mark.active = true;
 
-            this.initialVelocity = this.calculateVelocity(this.mark.getPosition(), this.shootPoint.getPosition(), this.flightDuration);
-            this.playerSprite.active = false;   
+            //this.initialVelocity = this.calculateVelocityPlayerLevelTarget(this.mark.getPosition(), this.shootPoint.getPosition(), this.flightDuration);
+            this.initialVelocity = this.calculateVelocityGroundLevelTarget(this.mark.getPosition(), this.shootPoint.getPosition(), this.flightDuration);
+            this.playerSprite.active = false;
             this.anim.playAdditive();
         }, this);
     }
@@ -60,7 +65,7 @@ export default class Target extends cc.Component {
         bullet.getComponent(cc.RigidBody).linearVelocity = this.initialVelocity;
     }
 
-    calculateVelocity(target: cc.Vec2, origin: cc.Vec2, time: number): cc.Vec2 {
+    calculateVelocityPlayerLevelTarget(target: cc.Vec2, origin: cc.Vec2, time: number): cc.Vec2 {
 
         //define distance x and y
         let distance: cc.Vec2 = target.sub(origin);
@@ -79,5 +84,18 @@ export default class Target extends cc.Component {
         result.y = Vy;
 
         return result;
+    }
+
+    calculateVelocityGroundLevelTarget(target: cc.Vec2, origin: cc.Vec2, time: number): cc.Vec2 {
+        // let R: number = this.mark.getPosition().x - this.shootPoint.getPosition().x;
+        let R: number = target.x - this.shootPoint.getPosition().x;
+        // let H: number = this.shootPoint.getPosition().y - this.groundY;
+        let H: number = origin.y - this.groundY;
+
+        let Vx: number = R / time;
+        let Vy: number = (0.5 * Math.abs(cc.director.getPhysicsManager().gravity.y * time * time) - H) / time;
+
+        let Vo = cc.v2(Vx, Vy);
+        return Vo;
     }
 }
